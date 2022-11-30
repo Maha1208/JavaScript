df3e0c9e723267db80e2947c1c61ec8464ec0701
Maha1208 <118327217+Maha1208@users.noreply.github.com>
2022-11-30 22:31:01 +0530

    Add files via upload

---
+++ StringHandling/Strings.js
@@ -0,0 +1,26 @@
+var a="I am learning java script ";
+var b="I'm learnt html, html and css html";
+var c="I'm going to learn React js";
+
+//charAt()
+console.log(a.charAt(8));
+
+//charCodeAt()
+console.log(a.charCodeAt(1));
+console.log(a.charCodeAt(a-1));
+
+//concatenation
+console.log(a+" "+"and" +" ".concat(b,c));
+console.log(a +" "+"and"+" "+c);
+
+//indexOf
+console.log(a.indexOf('j'));
+console.log(a.indexOf('a',4));
+console.log(b.indexOf("html"));
+console.log(b.indexOf("html",3));
+
+//lastIndexOf
+console.log();
+console.log();
+console.log();
+console.log();
